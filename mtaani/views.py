8f54c9a78c245aabf10d6a4635ddad96f7587f5b
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-06-19 14:29:58 +0300

    add methods for profile and to edit profile

--- mtaani/views.py
+++ mtaani/views.py
@@ -1,13 +1,36 @@
-import re
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from mtaani.models import *
+from .forms import *
 
 # Create your views here.
 
 def homepage(request):
     title =  "homepage"
+    posts = Post.objects.all()
+    neighbourhoods = Neighbourhood.objects.all()
+    profiles = Profile.objects.all()
     context = {
         "title": title,
+        "profiles": profiles,
+        "neighbourhoods": neighbourhoods,
+        "posts":posts,
     }
     return render(request, 'all-pages/homepage.html',context)
+
+
+def profile(request,username):
+    
+    return render(request, 'all-pages/profile.html')
+
+def edit_profile(request,username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile', user.username)
+    else:
+        form = UpdateProfileForm(instance=request.user.profile)
