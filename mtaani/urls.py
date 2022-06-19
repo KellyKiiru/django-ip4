2a459179545250cd0dd1227efb816b6821421c31
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-06-19 14:27:24 +0300

    add django-registration redux urls

--- mtaani/urls.py
+++ mtaani/urls.py
@@ -1,7 +1,14 @@
 from django. urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns =[
     path('',views.homepage,name='homepage'),
-    #path('',views.new,name='homepage'),
+    path('login/', auth_views.LoginView.as_view(),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(),name='logout'),\
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
